cf234059a99a41ecd388eba1b500a637830c5982
Seth McCullough <sethmckilladev@gmail.com>
2020-12-31 10:05:09 -0500

    Add memoization

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import jsonPlaceholder from "../api/jsonPlaceholder";
 
 export const fetchPosts = () => async (dispatch) => {
@@ -8,3 +9,14 @@ export const fetchPosts = () => async (dispatch) => {
     payload: response.data,
   });
 };
+
+export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({
+    type: "FETCH_USER",
+    payload: response.data,
+  });
+});
